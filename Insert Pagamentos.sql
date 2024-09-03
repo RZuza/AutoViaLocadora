53d45ee270655711dd770bfd7ca98413e8f086e9
Rikelmy Zuza <rikzuzas2@gmail.com>
2024-09-03 10:00:12 -0300

    Update Insert Pagamentos.sql

--- Insert Pagamentos.sql
+++ Insert Pagamentos.sql
@@ -1,3 +1,3 @@
-INSERT INTO Pagamentos (id_contrato, data_pagamento, valor, metodo) VALUES
-(1, '2024-08-01', 350.00, 'Cartão de Crédito'), 
+insert into Pagamentos (id_contrato,data_pagamento,valor,metodo) values
+(1, '2024-08-01', 350.00, 'Cartao de Credito'),
 (3, '2024-08-10', 400.00, 'Dinheiro');
